e78c53d39c74a0ee09eee3a75752779d20a5ff6b
hayderali3612 <hayderali3612@yahoo.com>
2022-02-02 18:39:18 +0300

    hha

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-const input = document.querySelector('#input');
-  const button = document.querySelector('#button');
-  const mainEle = document.querySelector('main.data')
+
+  const printButton = document.querySelectorAll('div.main-file-input > button');
+  const mainEle = document.querySelector('div.barcodes')
+
 let a = 0;
-button.onclick = function(){
-  
-  
-  const svgEle = document.createElement('svg');
-  svgEle.className = "barcode"+ a + "svg"
+const inputFile = document.querySelector('.input-flie')
+
+inputFile.addEventListener('change', () => {
+  const reader = new FileReader();
+  mainEle.innerHTML = ''
+  reader.onload = function(evt) {
+  const datas = JSON.parse(reader.result);
+  datas.map(data => {
+  const svgEle = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svgEle.className.baseVal = "barcode" + a;
   mainEle.append(svgEle)
-JsBarcode("main.data .barcode"+ a + "svg", input.value, {
-  lineColor: '#ECB365',
-});
-a++
+  JsBarcode('.barcode' + a, data.Id)
+  a++
+  })
+  printButton[1].removeAttribute("disabled")
+  printButton[2].removeAttribute("disabled")
+  mainEle.parentElement.classList.add('aa')
+  
+  };
+  reader.readAsText(inputFile.files[0])
+})
 
+printButton[1].addEventListener('click', () => {
+  print()
+})
 
-}
+printButton[2].addEventListener('click', () => {
+  mainEle.innerHTML = ''
+})
